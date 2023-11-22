f25998e70a6e74a7bfd4fc0fb08777b0966ab525
devipriyakakarla <priyakakarla97@gmail.com>
2023-11-22 11:06:42 +0530

    This is my second commit

--- app/src/main/java/com/hanuman/githubdemoproject/MainActivity.kt
+++ app/src/main/java/com/hanuman/githubdemoproject/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.hanuman.githubdemoproject
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textview = findViewById<TextView>(R.id.textview)
     }
 }
